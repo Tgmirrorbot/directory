7b9ef2564a9c54053eeabbe9e21c92d51d580398
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-11 15:40:58 +0530

    Commit from Stub AI (#1)
    Co-authored-by: Tgmirrorbot <bot@codestub.ai>

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-transparent w-full p-5 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">Discover Web App</h1>
+    </header>
+  );
+};
+
+export default Header;
