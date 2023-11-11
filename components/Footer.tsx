988ae890a53febbebc12b2856d75b8e47d5d4cc0
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-11 16:43:15 +0530

    Commit from Stub AI (#4)
    Co-authored-by: Tgmirrorbot <bot@codestub.ai>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FaTwitter, FaGithub, FaProductHunt } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="w-full p-5 flex justify-center items-center bg-transparent">
-      <p className="text-sm">© 2023 Discover Web App. All rights reserved.</p>
+    <footer className="w-full p-5 flex justify-center items-center bg-white dark:bg-gray-800">
+      <div className="flex space-x-4">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter className="text-blue-500 dark:text-white" size={24} />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="text-black dark:text-white" size={24} />
+        </a>
+        <a href="https://producthunt.com" target="_blank" rel="noopener noreferrer">
+          <FaProductHunt className="text-orange-500 dark:text-white" size={24} />
+        </a>
+      </div>
+      <p className="text-sm ml-4">© 2023 Discover Web App. All rights reserved.</p>
     </footer>
   );
 };
--- components/WebAppCard.tsx
+++ components/WebAppCard.tsx
@@ -8,10 +8,10 @@ type WebAppCardProps = {
 
 const WebAppCard: React.FC<WebAppCardProps> = ({ name, description, url }) => {
   return (
-    <div className="p-5 mb-5 border border-gray-200 rounded-lg">
-      <h2 className="mb-2 text-xl font-bold">{name}</h2>
-      <p className="mb-3 text-gray-600">{description}</p>
-      <a href={url} className="text-blue-500 hover:underline">Visit Site</a>
+    <div className="p-5 mb-5 border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800">
+      <h2 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">{name}</h2>
+      <p className="mb-3 text-gray-600 dark:text-gray-300">{description}</p>
+      <a href={url} className="text-blue-500 hover:underline dark:text-blue-400">Visit Site</a>
     </div>
   );
 };
