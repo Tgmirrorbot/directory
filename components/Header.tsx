ffad43ec423bc805190c12c3aa7d98088b817db7
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-11 15:48:26 +0530

    Commit from Stub AI (#3)
    Co-authored-by: Tgmirrorbot <bot@codestub.ai>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useTheme } from 'next-themes';
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <header className="w-full p-5 flex justify-between items-center bg-transparent">
+    <header className="w-full p-5 flex justify-between items-center bg-white dark:bg-gray-800">
       <h1 className="text-2xl font-bold">Discover Web App</h1>
+      <button
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        className="p-2 rounded bg-gray-600 text-white"
+      >
+        Switch to {theme === 'dark' ? 'light' : 'dark'} mode
+      </button>
     </header>
   );
 };
