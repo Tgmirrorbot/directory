ed0a609d25c04066c87787eb82786d7ec86f01fb
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-11 19:05:25 +0530

    Commit from Stub AI (#10)
    Co-authored-by: Tgmirrorbot <bot@codestub.ai>

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,15 +10,8 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ search, setSearch, filterPopular, setFilterPopular }) => {
   return (
     <header className="w-full p-5 flex justify-between items-center bg-white dark:bg-gray-800">
-      <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Discover Web App</h1>
       <div className="flex items-center space-x-4">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="rounded-md p-2"
-        />
+        <h1 className="text-xl font-bold text-gray-900 dark:text-white">GPTs</h1>
         <label className="flex items-center">
           <input
             type="checkbox"
@@ -28,6 +21,15 @@ const Header: React.FC<HeaderProps> = ({ search, setSearch, filterPopular, setFi
           />
           <span>Popular</span>
         </label>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="rounded-md p-2"
+        />
       </div>
     </header>
   );
