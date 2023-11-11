ffdc3c4a3c72d972534d88a2974179a334067d72
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-11 21:21:33 +0530

    Commit from Stub AI (#20)
    Co-authored-by: Tgmirrorbot <bot@codestub.ai>

--- components/WebAppCard.tsx
+++ components/WebAppCard.tsx
@@ -4,13 +4,13 @@ type WebAppCardProps = {
   name: string;
   description: string;
   url: string;
-  popular: boolean;
+  isPopular: boolean;
 };
 
-const WebAppCard: React.FC<WebAppCardProps> = ({ name, description, url, popular }) => {
+const WebAppCard: React.FC<WebAppCardProps> = ({ name, description, url, isPopular }) => {
   return (
     <div className="p-5 mb-5 border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 relative w-full sm:w-1/2 lg:w-1/4">
-      {popular && <span role="img" aria-label="fire" className="absolute top-2 right-2">🔥</span>}
+      {isPopular && <span role="img" aria-label="fire" className="absolute top-2 right-2">🔥</span>}
       <h2 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">{name}</h2>
       <p className="mb-3 text-gray-600 dark:text-gray-300">{description}</p>
       <a href={url} className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 dark:text-white">Visit Site</a>
